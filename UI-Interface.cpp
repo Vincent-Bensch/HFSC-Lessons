9991292834cdacc9ee04f2cb2eeb2661ce6b6723
Vincent Bensch <vbensch@gmail.com>
2016-06-18 22:40:07 -0500

    Launch Application Code

--- UI-Interface.cpp
+++ UI-Interface.cpp
@@ -1,2 +1,15 @@
 #include "UI-Interface.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	HFSCLessonSignup::StudentSearch form;
+	Application::Run(%form);
+}
